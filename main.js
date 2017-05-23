4efcca2c02c2a2078e2f59a4e9b1f3b9e59f22c1
iradkot <irad16@gmail.com>
2017-05-23 16:57:16 +0300

    you can buy drinks, money and supply

--- main.js
+++ main.js
@@ -1,32 +1,40 @@
 var coffeeShop = {
   beans: 40,
-
+  money: 100,
   drinkRequirements: {
-    latte: 10,
-    americano: 5,
-    doubleShot: 15,
-    frenchPress: 12
+    latte: {beansCost: 10, price: 20}, 
+    americano: {beansCost: 5, price: 10},
+    doubleShot: {beansCost: 15, price: 30},
+    frenchPress: {beansCost: 12, price: 24}
   },
-
-  makeDrink: function (drinkType) {
-    if(drinkType in coffeeShop.drinkRequirements) {
-      if(this.beans-this.drinkRequirements[drinkType]<0)  {
-        alert('sry no beans');
-      }
-      else  {
-        this.beans -= this.drinkRequirements[drinkType];
-        alert(this.beans);
-      }
-      
+  buySupplies: function(amount){
+    this.beans += amount;
+    this.money -= amount;
+  },
+  makeDrink: function(drinkType) {
+    if(!(drinkType in coffeeShop.drinkRequirements)) {
+      alert("Sorry, we don't make " + drinkType);
+      return false;
+    }
+    else if(this.beans-this.drinkRequirements[drinkType].beansCost<0)  {
+      alert('sry no beans');
+      return false;
     }
     else  {
-      alert("Sorry, we don't make " + drinkType);
+      this.beans -= this.drinkRequirements[drinkType].beansCost;
+      alert(this.beans);
+      return true;
+    }
+  },
+  buyDrink: function(drinkType) {
+    if(this.makeDrink(drinkType))  {
+      this.money+= this.drinkRequirements[drinkType].price;
     }
   }
 }
-
-coffeeShop.makeDrink("latte");
-coffeeShop.makeDrink("americano");
-coffeeShop.makeDrink("filtered");
-coffeeShop.makeDrink("doubleShot");
-coffeeShop.makeDrink("frenchPress");
+coffeeShop.buySupplies(10);
+coffeeShop.buyDrink("latte");
+coffeeShop.buyDrink("americano");
+coffeeShop.buyDrink("filtered");
+coffeeShop.buyDrink("doubleShot");
+coffeeShop.buyDrink("frenchPress");
